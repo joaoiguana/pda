67489885e533de9f8a7427793d4e7dd57d36bc3d
Joao Iguana <jiboiadamor@gmail.com>
2024-08-27 00:53:43 +0100

    creating a profile cards for personal digital assistants

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import ProfileCard from "./ProfileCard";
+import AlexaImage from './images/alexa.png';
+import CortanaImage from "./images/cortana.png";
+import SiriImage from "./images/siri.png";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>Personal Digital Assitants</div>
+
+      <ProfileCard title='Alexa' handle='@alexa99' image={AlexaImage} />
+      <ProfileCard title='Cortana' handle='@cortana32' image={CortanaImage} />
+      <ProfileCard title='Siri' handle='@siri01' image={SiriImage} />
     </div>
   );
 }
---
+++ src/ProfileCard.js
@@ -0,0 +1,12 @@
+const ProfileCard = ({ title, handle, image }) => {
+
+  return (
+    <div>
+      <img src={image} alt='pda-logo' />
+      <div>Title is {title}</div>
+      <div>Handle is {handle}</div>
+    </div>
+  )
+}
+
+export default ProfileCard;
